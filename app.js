6a8eab052e625e236daed77ad5b48e49669db5e9
Omar1997-SNHU <omar.a.bb@hotmail.com>
2026-03-16 07:58:43 -0700

    Module 2

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var hbs = require('hbs');
+
+var indexRouter = require('./app_server/routes/index');
+var travelRouter = require('./app_server/routes/travel');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
+
+// register handlebars partials
+hbs.registerPartials(path.join(__dirname, 'app_server/views/partials'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/travel', travelRouter);
+
+module.exports = app;
